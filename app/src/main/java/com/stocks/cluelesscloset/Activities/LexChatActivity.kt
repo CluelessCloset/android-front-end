b76d1f9740385d0a281f2f999722f31bea907585
DarrienG <darrienglasser@outlook.com>
2017-11-04 19:42:37 -0400

    WIP

--- app/src/main/java/com/stocks/cluelesscloset/Activities/LexChatActivity.kt
+++ app/src/main/java/com/stocks/cluelesscloset/Activities/LexChatActivity.kt
@@ -2,8 +2,10 @@ package com.stocks.cluelesscloset.Activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.amazonaws.auth.CognitoCredentialsProvider
 import com.amazonaws.mobileconnectors.lex.interactionkit.Response
 import com.amazonaws.mobileconnectors.lex.interactionkit.ui.InteractiveVoiceView
+import com.amazonaws.regions.Regions
 import com.stocks.cluelesscloset.R
 import java.lang.Exception
 
@@ -24,5 +26,19 @@ class LexChatActivity : AppCompatActivity(), InteractiveVoiceView.InteractiveVoi
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lex_chat)
 
+    }
+
+    private void init() {
+        appContext = getApplicationContext();
+        voiceView = (InteractiveVoiceView) findViewById(R.id.voiceInterface);
+        voiceView.setInteractiveVoiceListener(this);
+        CognitoCredentialsProvider credentialsProvider = new CognitoCredentialsProvider(
+                <your_conginto_identity_pool_id>,
+        Regions.fromName(<your_aws_region>)));
+        voiceView.getViewAdapter().setCredentialProvider(credentialsProvider);
+        voiceView.getViewAdapter().setInteractionConfig(
+                new InteractionConfig(<your_bot_name>),
+        <your_bot_alias>));
+        voiceView.getViewAdapter().setAwsRegion(<your_aws_region>));
     }
 }
